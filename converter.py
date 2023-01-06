a1b8bbe86c4a64e5b4e1780c9e60451e1b2b52ad
wisnuwdn <91248099+wisnuwdn@users.noreply.github.com>
2023-01-07 04:26:17 +0800

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,11 +1,3 @@
-# step 1: create new start symbol if start symbol occurs on rhs
-# step 2: remove null prod
-# step3: remove unit prod
-# step 4: replace each prod A->B1...Bn where n>2, with A->B1C where C-> B2...Bn.
-# repeat this step for all prod having two of more symbol on rhs
-# step 5: if any right siide of any prod is in the form A -> aB where a
-# is a terminal and A B are non terminals, replace terminal with X
-
 import init
 
 NEW_VAR = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
@@ -30,20 +22,6 @@ def remove_unit_prod():
                 grammar[key].extend(grammar[rule])
 
 
-def find_rule(exp):
-    #global grammar
-    if exp == []:
-        return []
-
-    tmp = []
-    for item in exp:
-        for key in grammar:
-            if item in grammar.get(key):
-                tmp.append(key)
-
-    return tmp
-
-
 def find_newrule(exp):
     if exp == '':
         return []
@@ -54,42 +32,14 @@ def find_newrule(exp):
     return []
 
 
-
 def remove_long_rhs():
     for key in grammar:
         if key not in terminals:
             rhs = grammar.get(key)
-            # print(rhs)
             for rule in rhs:
-                # print(rule, len(rule.split(" ")))
                 if len(rule.split(" ")) > 2:
-                    print(rule)
                     grammar[key].remove(rule)
                     grammar[key].extend([breakdown(rule)])
-
-    for key in new_prod:
-        grammar[key] = new_prod.get(key)
-    # for key in grammar:
-    #     if key not in terminals:
-    #         rhs = grammar.get(key)
-    #         for rule in rhs:
-    #             if len(rule.split(" ")) > 2:
-    #                 grammar[key].remove(rule)
-    #                 grammar[key].extend([breakdown(rule)])
-    #
-    # for key in new_prod:
-    #     grammar[key] = new_prod.get(key)
-
-
-def remove_long_k():
-    key = 'K'
-    rhs = grammar.get(key)
-    #print(rhs)
-    for rule in rhs:
-        #print(rule, len(rule.split(" ")))
-        if len(rule.split(" ")) > 2:
-            grammar[key].remove(rule)
-            grammar[key].extend([breakdown(rule)])
 
     for key in new_prod:
         grammar[key] = new_prod.get(key)
@@ -114,24 +64,23 @@ def breakdown(s): # 'S P Pel'
     return s
 
 
-remove_duplicate_rhs()
-#print('K', grammar.get('K'))
+def convert():
+    remove_duplicate_rhs()
 
-#remove_long_rhs()
-remove_long_k()
-remove_long_k()
-remove_long_k()
+    remove_long_rhs()
+    remove_long_rhs()
+    remove_long_rhs()
 
-remove_unit_prod()
+    remove_unit_prod()
+    remove_unit_prod()
+    remove_unit_prod()
 
-cfg_file = 'cnf.txt'
-with open(cfg_file, 'w') as newfile:
-    for k in grammar:
-        newfile.write(f"{k} -> ")
-        for obj in grammar.get(k):
-            newfile.write(f"{obj}")
-            if obj != grammar.get(k)[-1]:
-                newfile.write("|")
-        newfile.write("\n")
-
-
+    cfg_file = 'cnf.txt'
+    with open(cfg_file, 'w') as newfile:
+        for k in grammar:
+            newfile.write(f"{k} -> ")
+            for obj in grammar.get(k):
+                newfile.write(f"{obj}")
+                if obj != grammar.get(k)[-1]:
+                    newfile.write("|")
+            newfile.write("\n")
