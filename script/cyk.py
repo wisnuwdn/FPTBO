f0bdd2756d563d9dbca9a401115da707856db62e
fylnnx <101488449+fylnnx@users.noreply.github.com>
2023-01-07 11:25:10 +0800

    Update cyk.py

--- script/cyk.py
+++ script/cyk.py
@@ -37,43 +37,55 @@ def cyk(s):
     table = [[[] for i in range(n)] for j in range(n)] # buat tabel
 
     # loop untuk row 1
+    word_notfound = []
     for i in range(n):
+        if find_rule([s[i]]) == []:
+            word_notfound.append(s[i])
         table[i][i] = find_rule([s[i]])
 
-    # rumus cyk: V(i,j) = V(i-k[-(x)],j) * V(i, j+k[x-1])
-    # where k = range(1,loop)
-    # where x = range(loop)
+    if word_notfound !=[]:
+        str_notfound = ''
+        for i in word_notfound:
+            str_notfound += i
+            if i != word_notfound[-1] and len(word_notfound) !=1:
+                str_notfound += ', '
+        st.write("Kata", str_notfound, "tidak ada dalam CFG")
 
-    # n = 5
-    for loop in range(2, n+1): # 2, 3, 4, 5
-        k = range(1, loop) # [1]
-        for i in range(loop-1, n): # 1, 2, 3, 4
-            for j in range(n - loop+1): # 0, 1, 2, 3
-                for x in range(1, loop):
-                    table[i][j] += find_rule(concat(table[i-k[-x]][j], table[i][j+k[x-1]]))
-                    # cetak tabel masing-masing sel
+    elif word_notfound == []:
+        # rumus cyk: V(i,j) = V(i-k[-(x)],j) * V(i, j+k[x-1])
+        # where k = range(1,loop)
+        # where x = range(loop)
 
-        # cetak tabel per row
+        # n = 5
+        for loop in range(2, n + 1):  # 2, 3, 4, 5
+            k = range(1, loop)  # [1]
+            for i in range(loop - 1, n):  # 1, 2, 3, 4
+                for j in range(n - loop + 1):  # 0, 1, 2, 3
+                    for x in range(1, loop):
+                        table[i][j] += find_rule(concat(table[i - k[-x]][j], table[i][j + k[x - 1]]))
+                        # cetak tabel masing-masing sel
+                    table[i][j] = list(set(table[i][j]))
+            # cetak tabel per row
+            # check output in terminal
+            print(table)
+            # show CYK in web
+            st.dataframe(table)
+
+        st.write("Hasil Parsing CYK Table Filling: ")
+        # cetak tabel setelah selesai
+        print('\n\n')
+        table2 = []
+        for row in table:
+            table2.append(row)
         # check output in terminal
-        print(table)
+        print(table2)
         # show CYK in web
-        st.dataframe(table)
+        st.dataframe(table2)
 
-    st.write("Hasil Parsing CYK Table Filling: ")
-    # cetak tabel setelah selesai
-    print('\n\n')
-    table2 = []
-    for row in table:
-        table2.append(row)
-    #check output in terminal
-    print(table2)
-    #show CYK in web
-    st.dataframe(table2)
+        print('\n\n')
+        if table[n - 1][0] == []:
+            st.error("Kalimat tidak valid!")
+        else:
+            st.success("Kalimat valid")
 
-    print('\n\n')
-    if table[n-1][0] == []:
-        st.error("Kalimat tidak valid!")
-    else:
-        st.success("Kalimat valid")
-
-    return table
+        return table
